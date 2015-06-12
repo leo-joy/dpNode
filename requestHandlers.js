c9ad5746c2113f9983187c87fd85f37a2574f9e3
leo-joy <small_ant@sina.cn>
2015-06-12 17:26:10 +0800

    新增redis模块

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,6 +2,9 @@
 var querystring = require("querystring");
 var fs = require("fs");
 var formidable= require("formidable");
+var redis = require('redis');
+var http = require('http');
+var url = require("url");
 function startup(response, request) {
 	console.log("Request handler 'startup' was called.");
 	
@@ -65,11 +68,41 @@ function show(response, request) {
 	});
 }
 
-function getJson(response, request) {
-	response.setHeader("Access-Control-Allow-Origin", "*");
+function getJson(response, req) {
+	
+	// 设置接收数据编码格式为 UTF-8
+    req.setEncoding('utf-8');
+    var postData = ""; //POST & GET ： name=zzl&email=zzl@sina.com
+    // 数据块接收中
+    req.addListener("data", function (postDataChunk) {
+        postData += postDataChunk;
+    });
+    // 数据接收完毕，执行回调函数
+    req.addListener("end", function () {
+        console.log('数据接收完毕');
+        var params = querystring.parse(postData);//GET & POST  ////解释表单数据部分{name="zzl",email="zzl@sina.com"}
+        console.log(params);
+        console.log(params["name"]);
+        PushToRedis(params["name"]);
+        response.writeHead(500, {
+            "Content-Type": "text/plain;charset=utf-8"
+        });
+        response.end("数据提交完毕");
+    });
+    
+	//表单接收完成后，再处理redis部分
+	function PushToRedis(info) {
+	    var client = redis.createClient();
+	    client.lpush("topnews", info);
+	    console.log("PushToRedis:" + info);
+	    client.lpop("topnews", function (i, o) {
+	        console.log(o);//回调，所以info可能没法得到o的值，就被res.write输出了
+	    })
+	    client.quit();
+	}
+	response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8020");
     response.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
     response.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    response.setHeader("X-Powered-By",' 3.2.1')
 	response.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
 	
 	response.write('{"name":"pde"}');
